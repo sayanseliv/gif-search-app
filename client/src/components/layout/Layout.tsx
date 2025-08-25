4f9d6784a4873918341d322f309dacab2d10f8af
Eugen <eugen.lang@zelh.com>
2025-08-25 23:23:39 +0300

    init dependencies

---
+++ client/src/components/layout/Layout.tsx
@@ -0,0 +1,16 @@
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = () => {
+	return (
+		<>
+			<Header />
+			<main className='min-h-screen w-full px-4 py-8 bg-gray-100'>
+				<div className='max-w-7xl mx-auto w-full'></div>
+			</main>
+			<Footer />
+		</>
+	);
+};
+
+export default Layout;
